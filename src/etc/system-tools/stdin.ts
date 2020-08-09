55d70c4d9b306fd7e0f8f885c9973c49e35c0f23
Grace Punzalan <grace@Graces-MacBook-Pro.local>
2020-08-09 03:01:13 -0400

    added cli updates

--- src/etc/system-tools/stdin.ts
+++ src/etc/system-tools/stdin.ts
@@ -1,12 +1,37 @@
 import readline from 'readline';
+import { toUnicode } from "./string-toUnicode";
 
-export function userInput(question:string):customStdinResponse {
+export let cliStack: string[] = [];
+
+export function userInput(question: string): customStdinResponse {
+    process.stdin.setEncoding('utf8');
+    
+    let cliStackIndex = cliStack.length;
+    
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     });
-    let promise =  new Promise(resolve =>{
-        rl.question(question, (ans:any) => {
+    
+    process.stdin.on('data', listener);
+    async function listener(key:any) {
+        if (toUnicode(key) == "\\u001B\\u005B\\u0041") {
+            writer((cliStackIndex < 1) ? 0: cliStackIndex - 1);
+        } else if (toUnicode(key) == '\\u001B\\u005B\\u0042') {
+            writer((cliStackIndex < cliStack.length) ? cliStackIndex + 1: cliStack.length);
+        } else {
+            process.stdin.removeListener('data', listener);
+        }  
+    }
+
+    function writer(index:number){
+        cliStackIndex = index
+        rl.write(">>", { ctrl: true, name: 'u' });
+        rl.write(cliStack[index]);  
+    }
+
+    let promise = new Promise(resolve => {
+        rl.question(question, (ans: any) => {
             rl.close();
             resolve(ans);
         })
